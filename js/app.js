cb9e3b18f7e6fdd9e045250a1530ccb5e0ae16a1
ripo <ripo@nologin.es>
2014-07-01 15:30:38 +0200

    Fix POST as JSON and use correct params

--- js/app.js
+++ js/app.js
@@ -1,5 +1,22 @@
 jQuery(function($){
-	$('#sec-1').tooltip();
+	//('#sec-1').tooltip();
+
+	$.fn.serializeObject = function()
+	{
+		var o = {};
+		var a = this.serializeArray();
+		$.each(a, function() {
+			if (o[this.name] !== undefined) {
+				if (!o[this.name].push) {
+					o[this.name] = [o[this.name]];
+				}
+				o[this.name].push(this.value || '');
+			} else {
+				o[this.name] = this.value || '';
+			}
+		});
+		return o;
+	};
 	$(document).ready(function(){
 		$('#msgbox').dialog({
 				autoOpen: false,
@@ -15,7 +32,7 @@ jQuery(function($){
 		$('#bot').on('click',function(){
 			event.preventDefault();
 			if($('#user_login').val() !== '' && $('#pass_login').val() !== ''){
-				$.post(this.form.action,$(this.form).serialize())
+				$.post(this.form.action,JSON.stringify($(this.form).serializeObject()))
 				.done(function(data){
 					console.log('ok');
 				})
